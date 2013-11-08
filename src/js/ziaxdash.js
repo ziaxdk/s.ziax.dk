8d53111b721e70549ee2bfd44ec209a16d5c69b0
ziax <ziaxdk@gmail.com>
2013-11-09 00:24:10 +0100

    moved html

--- src/js/ziaxdash.js
+++ src/js/ziaxdash.js
@@ -3,22 +3,22 @@ var module = angular.module('ziaxdash', ['ngRoute', 'ngResource', 'ngAnimate']);
 // Config
 module.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/', {
-      templateUrl: "_index.html",
+      templateUrl: "/html/_index.html",
       resolve: { History: ['$http', function($http) { return $http.get('/api/history'); }] },
       controller: "IndexController",
       controllerAs: "IndexCtrl"
   });
   $routeProvider.when('/new', {
-      templateUrl: "_new.html"
+      templateUrl: "/html/_new.html"
   });
   $routeProvider.when('/show/:id', {
-      templateUrl: "_show.html",
+      templateUrl: "/html/_show.html",
       resolve: { Drive: ['$route', 'RestQ', function($route, RestQ) { return RestQ.save({id: $route.current.params.id }); }] },
       controller: "ShowController",
       controllerAs: "ShowCtrl"
   });
   $routeProvider.when('/res/:q', {
-      templateUrl: "_result.html",
+      templateUrl: "/html/_result.html",
       resolve: { Drives: ['$route', 'RestQ', function($route, RestQ) { return RestQ.get({ q: $route.current.params.q }); }] },
       controller: "ResultController",
       controllerAs: "ResultCtrl"
