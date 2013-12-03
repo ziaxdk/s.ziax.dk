70e2e9cbaae2542576fd45d0fd9fe565ece68084
ziaxdk <ziaxdk@gmail.com>
2013-12-03 10:20:50 +0100

    changed index.html

--- src/js/ziaxdash.js
+++ src/js/ziaxdash.js
@@ -40,10 +40,15 @@ module.controller('IndexController', ['History', 'LocationService', '$location',
   function (History, LocationService, $location, $scope) {
   var _t = this;
   _t.history = History.data.facets.history.terms;
+  _t.showInfo = false;
   
 
   _t.search = function (q) {
     $location.path('res/' +  encodeURIComponent(q.term));
+  };
+
+  _t.toggle = function () {
+    _t.showInfo = !_t.showInfo;
   };
 
   $scope.$watch(function () {return LocationService.coords; }, function (n) {
