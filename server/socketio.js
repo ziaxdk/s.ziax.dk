f58032cd9f35836cee55f159ce74819cb3270845
ziax <ziaxdk@gmail.com>
2014-04-24 16:20:07 +0200

    .

--- server/socketio.js
+++ server/socketio.js
@@ -1,25 +1,23 @@
-(function () {
-  var SIO = require('./../server.js').sio
-    , esDocument = require('./es-document.js');
+var SIO = require('./../server.js').sio,
+    esDocument = require('./es-document.js');
 
-  SIO.sockets.on('connection', function (socket) {
-    // console.log('A socket connected!');
-    esDocument.count().then(function(data) {
-      // count = data.count;
-      socket.emit('connect', data);
-    });
+SIO.sockets.on('connection', function (socket) {
+  // console.log('A socket connected!');
+  esDocument.count().then(function(data) {
+    // count = data.count;
+    socket.emit('connect', data);
   });
+});
 
-  function status() {
-    console.log(SIO);
-  }
+function status() {
+  console.log(SIO);
+}
 
-  function plusOne() {
+function plusOne() {
 
-  }
+}
 
-  module.exports = {
-    plusOne: plusOne,
-    status: status
-  };
-}());
+module.exports = {
+  plusOne: plusOne,
+  status: status
+};
